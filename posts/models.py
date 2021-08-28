bfef43618abfc2323334a136258285ed49becf9d
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-08-27 22:23:16 -0300

    Ajustes no input de imagens na seção admin e ajustes na responsividade na home para mobile.

--- posts/models.py
+++ posts/models.py
@@ -5,14 +5,14 @@ from django.utils import timezone
 from categorias.models import Categoria
 
 class Post(models.Model):
-    titulo_post = models.CharField(max_length=255, verbose_name='Título post')
+    titulo_post = models.CharField(max_length=150, verbose_name='Título post')
     categoria_post = models.ForeignKey(Categoria, blank=True, null=True,
                                        on_delete=models.DO_NOTHING, verbose_name='Categoria post')
     autor_post = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Autor do post')
     data_post = models.DateTimeField(auto_now_add=True, verbose_name='Data do post')
-    excerto_post = models.TextField(verbose_name='Excerto')
+    excerto_post = models.CharField(max_length=200, verbose_name='Excerto')
     conteudo_post = models.TextField(verbose_name='Conteúdo do post')
-    imagem_post = models.ImageField(upload_to='post_img/%Y/%m/%d', verbose_name='Imagem', blank=True, null=True)
+    imagem_post = models.CharField(max_length=255, verbose_name='Link da imagem do post', blank=True, null=True)
     publicado_post = models.BooleanField(default=False, verbose_name='Publicado')
     posts = models.Manager()
 

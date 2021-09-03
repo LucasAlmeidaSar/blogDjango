c2c7d744dfa544346855a2a9675a7ce1ef8392e5
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-09-02 21:10:19 -0300

    Ajuste no nome do title da tela admin e estilizando a mensagem de erro de envio de formulário.

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.index_title = 'Lucas Almeida - Site '
+admin.site.index_title = 'Gerenciar site'
 
 
 urlpatterns = [
